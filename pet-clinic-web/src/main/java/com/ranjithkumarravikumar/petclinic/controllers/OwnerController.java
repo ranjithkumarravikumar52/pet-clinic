e8ace63f10f716dc5d15555448f725d729c99155
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-27 08:27:56 -0400

    added owners list on owners index page. Closes #14.

--- pet-clinic-web/src/main/java/com/ranjithkumarravikumar/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ranjithkumarravikumar/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.ranjithkumarravikumar.petclinic.controllers;
 
+import com.ranjithkumarravikumar.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+	private final OwnerService ownerService;
+
+	@Autowired //optional
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping("/index")
-	public String listOwners(){
+	public String listOwners(Model model){
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
