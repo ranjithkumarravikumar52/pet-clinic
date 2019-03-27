4c625be5a6e6201a7b93e98ff761a985aff82b84
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-27 07:47:47 -0400

    added owner controller and index page. Closes #11.

---
+++ pet-clinic-web/src/main/java/com/ranjithkumarravikumar/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.ranjithkumarravikumar.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+	@RequestMapping("/index")
+	public String listOwners(){
+		return "owners/index";
+	}
+}
