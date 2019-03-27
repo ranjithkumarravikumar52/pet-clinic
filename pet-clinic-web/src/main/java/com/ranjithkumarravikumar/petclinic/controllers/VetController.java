6e1050d4c5f2e7a2295914cc0ef4e2463685ba02
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-27 08:36:59 -0400

    added vets list on vets index page. Closes #15.

--- pet-clinic-web/src/main/java/com/ranjithkumarravikumar/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ranjithkumarravikumar/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package com.ranjithkumarravikumar.petclinic.controllers;
 
+import com.ranjithkumarravikumar.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+	private final VetService vetService;
+
+	@Autowired //optional
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping("/index")
-	public String listVets(){
+	public String listVets(Model model){
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
