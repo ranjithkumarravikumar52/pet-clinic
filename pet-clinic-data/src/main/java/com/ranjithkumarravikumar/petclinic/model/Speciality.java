a8d3fbe05ad0b733b64e03c279c36a7b59c29090
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-28 06:03:15 -0400

    Updated Vet and Speciality with JPA entity mapping. Closes #34.

--- pet-clinic-data/src/main/java/com/ranjithkumarravikumar/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/ranjithkumarravikumar/petclinic/model/Speciality.java
@@ -3,8 +3,15 @@ package com.ranjithkumarravikumar.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
+	@Column(name = "description")
 	private String description;
 }
