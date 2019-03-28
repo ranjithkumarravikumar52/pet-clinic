3ad24092365d340ce0bfc00c030c213553c05d66
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-28 05:42:55 -0400

    implemented base entity with JPA annotations. Closes #32.

--- pet-clinic-data/src/main/java/com/ranjithkumarravikumar/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ranjithkumarravikumar/petclinic/model/BaseEntity.java
@@ -3,10 +3,17 @@ package com.ranjithkumarravikumar.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass //this tells jpa that we inherit from this class
 public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //mysql will provide us next sequence number
 	private Long id;
 }
