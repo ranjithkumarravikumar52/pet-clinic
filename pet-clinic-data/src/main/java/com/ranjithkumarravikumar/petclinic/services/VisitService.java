5096729ef53b6eb7600b43e0988d5b9c06c39e2e
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-28 11:03:46 -0400

    added map impl for visit service. Closes #42.

---
+++ pet-clinic-data/src/main/java/com/ranjithkumarravikumar/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.ranjithkumarravikumar.petclinic.services;
+
+import com.ranjithkumarravikumar.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
